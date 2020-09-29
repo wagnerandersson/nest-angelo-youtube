e849571c294571ba7e78ff5caf9c6dbd9eb45e5a
Felinto <emersong20.email@gmail.com>
2020-09-29 20:51:28 -0300

    🏗 the userService should be called with readonly

--- src/users/user.resolver.ts
+++ src/users/user.resolver.ts
@@ -7,41 +7,41 @@ import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver('User')
 export class UserResolver {
-    constructor(
-        private userService: UserService
-    ){}
+  constructor(
+    private readonly userService: UserService
+  ) { }
 
-    @Query(() => [User])
-    async users(): Promise<User[]> {
-        return await this.userService.findAllUsers();
-    }
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    return await this.userService.findAllUsers();
+  }
 
-    @Query(() => User)
-    async user(
-        @Args('id') id: string
-    ): Promise<User> {
-        return this.userService.getUserById(id);
-    }
+  @Query(() => User)
+  async user(
+    @Args('id') id: string
+  ): Promise<User> {
+    return this.userService.getUserById(id);
+  }
 
-    @Mutation(() => User)
-    async createUser(
-        @Args('data') data: CreateUserInput
-    ): Promise<User> {
-        return this.userService.createUser(data);
-    }
+  @Mutation(() => User)
+  async createUser(
+    @Args('data') data: CreateUserInput
+  ): Promise<User> {
+    return this.userService.createUser(data);
+  }
 
-    @Mutation(() => User)
-    async updateUser(
-        @Args('data') data: UpdateUserInput
-    ): Promise<User> {
-        return this.userService.updateUser(data);
-    }
+  @Mutation(() => User)
+  async updateUser(
+    @Args('data') data: UpdateUserInput
+  ): Promise<User> {
+    return this.userService.updateUser(data);
+  }
 
-    @Mutation(() => Boolean)
-    async deleteUser(
-        @Args('id') id: string
-    ): Promise<true> {
-        await this.userService.deleteUser(id);
-        return true;
-    }
+  @Mutation(() => Boolean)
+  async deleteUser(
+    @Args('id') id: string
+  ): Promise<true> {
+    await this.userService.deleteUser(id);
+    return true;
+  }
 }
